266e4a9d8788d651a590bafd43a63ac9befdb5e0
Etienne Fuh <36563272+THISmann@users.noreply.github.com>
2021-09-08 09:28:23 +0100

    clear code

--- server.js
+++ server.js
@@ -1,46 +1,3 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
-// const socketio = require('socket.io') 
-// const server = app.listen(3000,console.log("Socket.io Hello World server started!"));
-// const io = require('socket.io')(server);
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// // app.listen(port, () => {
-// //   console.log(`Example app listening at http://localhost:${port}`)
-// // })
-
-// app.get('/test' , (req , res)=> {
-//     res.send("I Love you");
-
-//     io.on('connection', (socket) => {
-//         //console.log("Client connected!");
-//         socket.on('message-from-client-to-server', (msg) => {
-//             console.log(msg);
-//         })
-//         socket.emit('message-from-server-to-client', 'Hello World!');
-//     });
-
-// })
-
-// app.get('/like', (req , res)=>{ 
-//     res.send('bonjour etienne fuh');
-//     console.log('hello dear !');
-// })
-
-// app.get('/file', (req , res)=>{
-//     res.sendFile(__dirname + '/index.html');
-// })
-
-// app.post('/envoi' ,(req , res)=> {
-//     res.sendFile(__dirname + '/form.html');
-// })
-
-
 const express = require('express'); 
 const app = express();
 const server = app.listen(3000,console.log("Socket.io Hello World server started!"));
@@ -53,9 +10,8 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-    console.log("Client connected!");
-    // socket.on('message-from-client-to-server', (msg) => {
-    //     console.log(msg);
+    console.log("Client connected!"); 
+    
     socket.on('disconnect', socket => {
         console.log("user  disconneted ")
     })
