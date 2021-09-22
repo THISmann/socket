475aa222403de373d176cbe87ed5af3114fa86c9
THISmann <fuhetienne0@gmail.com>
2021-09-22 04:34:31 +0100

    review

--- server.js
+++ server.js
@@ -1,61 +1,30 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
-// const socketio = require('socket.io') 
-// const server = app.listen(3000,console.log("Socket.io Hello World server started!"));
-// const io = require('socket.io')(server);
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// // app.listen(port, () => {
-// //   console.log(`Example app listening at http://localhost:${port}`)
-// // })
-
-// app.get('/test' , (req , res)=> {
-//     res.send("I Love you");
-
-//     io.on('connection', (socket) => {
-//         //console.log("Client connected!");
-//         socket.on('message-from-client-to-server', (msg) => {
-//             console.log(msg);
-//         })
-//         socket.emit('message-from-server-to-client', 'Hello World!');
-//     });
-
-// })
-
-// app.get('/like', (req , res)=>{ 
-//     res.send('bonjour etienne fuh');
-//     console.log('hello dear !');
-// })
-
-// app.get('/file', (req , res)=>{
-//     res.sendFile(__dirname + '/index.html');
-// })
-
-// app.post('/envoi' ,(req , res)=> {
-//     res.sendFile(__dirname + '/form.html');
-// })
-
+//=============================================================================
+//   DECLARE ALL THE VARIABLE FOR THE Applications 
+//============================================================================= 
 
 const express = require('express'); 
 const app = express();
 const server = app.listen(3000,console.log("Socket.io Hello World server started!"));
 const io = require('socket.io')(server);
-
-
+var data = {
+    name: 'etienne',
+    prenom: 'fuh',
+    data: '11-12-2022'
+};
+//=============================================================================
+//  ROUTAGE FOR THE APP 
+//============================================================================= 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+//=============================================================================
+// INITIALIZE THE SOCKET 
+//=============================================================================
+
 io.on('connection', (socket) => {
-    console.log("Client connected!");
-    // socket.on('message-from-client-to-server', (msg) => {
-    //     console.log(msg);
+    console.log("Client connected!"); 
     socket.on('disconnect', socket => {
         console.log("user  disconneted ")
     })
@@ -64,6 +33,11 @@ io.on('connection', (socket) => {
         console.log('message recu : ' + msg);
         io.emit('chat' , msg);
     })
+
+    socket.on('data' , (data) => {
+        io.emit('data', data);
+    })
+    
 })
 
  
